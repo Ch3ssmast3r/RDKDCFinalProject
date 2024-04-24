cfc97c1a92e832fb0121d5fe5358a093b92306dc
andrewp0415 <81529989+andrewp0415@users.noreply.github.com>
2024-04-24 17:44:37 -0400

    asdf

--- lab3.m
+++ lab3.m
@@ -7,7 +7,7 @@
 R1 = eye(3); %translational case
 p1 = [2;1;0];
 
-R2 = ROTZ(pi)*ROTY(.5);
+R2 = ROTZ(1)*ROTX(pi/2);
 p2 = [-1;0;1];
 
 R3 = ROTY(pi/4)*ROTX(pi/2)*ROTZ(1);
@@ -23,9 +23,26 @@ xi1 = getXi(g1);
 xi2 = getXi(g2);
 xi3 = getXi(g3);
 
+%extract v and w
+v1 = xi1(1:3);
+w1 = xi1(4:6);
+xiHat1 = [SKEW3(w1),v1;0,0,0,0];
+
+v2 = xi2(1:3);
+w2 = xi2(4:6);
+xiHat2 = [SKEW3(w2),v2;0,0,0,0];
+
+v3 = xi3(1:3);
+w3 = xi3(4:6);
+xiHat3 = [SKEW3(w3),v3;0,0,0,0];
+
 %calculate g again from the twists, then see if they match. This will be
 %done using the function "XiToG" which takes a twist and finds the
 %homogeneous transformation.
-gOne = XiToG(xi1);
-gTwo = XiToG(xi2);
-gThree = XiToG(xi3);
+gOne = expm(xiHat1);
+gTwo = expm(xiHat2);
+gThree = expm(xiHat3);
+
+error1 = g1-gOne
+error2 = g2-gTwo
+error3 = g3-gThree
