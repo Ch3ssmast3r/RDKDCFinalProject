f047849206674cca13a87ee91269de23a8b62614
andrewp0415 <81529989+andrewp0415@users.noreply.github.com>
2024-04-24 09:18:30 -0400

    Update getXi.m

--- getXi.m
+++ getXi.m
@@ -1,19 +1,25 @@
 function[xi] = getXi(g)
-%g is the homogeneous transformation matrix
-R = g(1:3,1:3);
-theta = acos((trace(R)-1)/2);
-p = g(1:3,4);
-if theta == 0
+%g is the homogeneous transformation matrix in SE(3)
+R = g(1:3,1:3); %extract the rotation matrix from g
+theta = acos((trace(R)-1)/2); %theta is dependent upon the rotation matrix
+p = g(1:3,4); %extract the position vector from g
+
+%We must account for two cases: 1) purely translational and 2) general case
+if theta == 0 %purely translational case. v is simply dependent on the p vector.
     omega = [0;0;0];
     v = p/norm(p);
     theta = norm(p);
-else
-    diff = R-transpose(R);
+else %more general case. 
+    diff = R-transpose(R); %This is needed to calculate omega, using the general equation
+    %omega = (R-R^T)^V/(2sin(theta))
     differenceV = [diff(3,2);diff(1,3);diff(2,1)];
     omega = differenceV/(2*sin(theta));
+    %next we compute v. the equation is summarized in this matrix (A) such
+    %that vA = p.
     omegaHat = SKEW3(omega);
     A = eye(3)*theta + (1-cos(theta))*omegaHat +...
         (theta - sin(theta))*omegaHat*omegaHat;
-    v = inv(A)*p;
+    v = inv(A)*p; %solve for v
 en
-xi = theta*[v;omega];
+xi = theta*[v;omega]; %we are interested in a non-normalized xi, which is why we...
+%multiply the unit twist by theta.
