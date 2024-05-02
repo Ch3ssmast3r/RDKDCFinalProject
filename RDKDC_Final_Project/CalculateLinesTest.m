91128cb189e051328fc06c89e0f36ccafb915aaf
andrewp0415 <81529989+andrewp0415@users.noreply.github.com>
2024-05-02 12:40:50 -0400

    asdf

--- RDKDC_Final_Project/CalculateLinesTest.m
+++ RDKDC_Final_Project/CalculateLinesTest.m
@@ -6,7 +6,13 @@
 g1 = [eye(3),[1;.5;.5];0,0,0,1];
 g4 = [eye(3),[1+.1*.707;.5-.1*.707;.75];0,0,0,1];
 
-[g1, g2, g3, g4] = ur5_calculate_lines(g1,g4);
+frames = ur5_calculate_lines(g1,g4);
+
+%extract the four frames
+g1 = frames(:,:,1);
+g2 = frames(:,:,2);
+g3 = frames(:,:,3);
+g4 = frames(:,:,4);
 
 %extract our four points
 v1 = g1(1:3,4);
@@ -14,10 +20,10 @@ v2 = g2(1:3,4);
 v3 = g3(1:3,4);
 v4 = g4(1:3,4);
 
-figure (1)
-hold on
-plotp3(v1)
-plotp3(v2)
-plotp3(v3)
-plotp3(v4)
-hold off
+% figure (1)
+% hold on
+% plotp3(v1)
+% plotp3(v2)
+% plotp3(v3)
+% plotp3(v4)
+% hold off
--- RDKDC_Final_Project/ur5_calculate_lines.m
+++ RDKDC_Final_Project/ur5_calculate_lines.m
@@ -1,4 +1,4 @@
-function[g1 g2 g3 g4] = ur5_calculate_lines(gstart,gfinish)
+function frames = ur5_calculate_lines(gstart,gfinish)
     %Andrew Palacio
     
     %this function inputs the frame transformations ALREADY INCLUDING the pen
@@ -6,11 +6,19 @@ function[g1 g2 g3 g4] = ur5_calculate_lines(gstart,gfinish)
     %straight lines. This will then output the frame transformations STILL
     %INCLUDING the pen tip transformation for the points 2 and 3.
     
+    %NECESSARY CHANGES
+    %ask the user to confirm that they like the two selected points
+    
     %we'll need this vector
     e3 = [0;0;1];
     
-    %we will keep orientation constant from the final pen position. 
-    R = gfinish(1:3,1:3);
+    %Go to this starting configuration in RPY form:
+    %Rx = -180
+    %Ry = 0
+    %Rz = 135
+    %the order is Rzyx
+    
+    R = ROTZ(3*pi/4)*ROTY(0)*ROTX(-pi);
     
     %extract the vectors from the homogeneous frames
     v1 = gstart(1:3,4); 
@@ -27,6 +35,22 @@ function[g1 g2 g3 g4] = ur5_calculate_lines(gstart,gfinish)
     
     %compute the distance between v1 and v4, projected in the xy plane.
     v14 = v4_xy - v1_xy;
+    
+    %make sure that points 1 and 4 aren't the same. If so, break the
+    %function.
+    if norm(v14)< .01 %threshold set to 1cm
+        disp('Error - Start and end positions are too close');
+        g1 = NaN(4);
+        g2 = NaN(4);
+        g3 = NaN(4);
+        g4 = NaN(4);
+        
+        frames(:,:,1) = g1;
+        frames(:,:,2) = g2;
+        frames(:,:,3) = g3;
+        frames(:,:,4) = g4;
+        return
+    end
     
     %make it a unit vector
     v14 = v14/norm(v14);
@@ -46,4 +70,32 @@ function[g1 g2 g3 g4] = ur5_calculate_lines(gstart,gfinish)
     %frames g1 and g4 will be re-expressed with the average z height.
     g1 = [R,v1_xy;0,0,0,1];
     g4 = [R,v4_xy;0,0,0,1];
+    
+    %plot the proposed points and ask if the user likes them
+    figure (1)
+    hold on
+    plotp3(v1_xy)
+    plotp3(v2)
+    plotp3(v3)
+    plotp3(v4_xy)
+    hold off
+    
+    accept = input('Is this the desired position? Input 1 for YES, 0 for NO. ');
+    if accept == 1
+        frames(:,:,1) = g1;
+        frames(:,:,2) = g2;
+        frames(:,:,3) = g3;
+        frames(:,:,4) = g4;
+    else 
+        disp('Error - User denied the proposed positions.');
+        g1 = NaN(4);
+        g2 = NaN(4);
+        g3 = NaN(4);
+        g4 = NaN(4);
+        
+        frames(:,:,1) = g1;
+        frames(:,:,2) = g2;
+        frames(:,:,3) = g3;
+        frames(:,:,4) = g4;
+    end
 end
