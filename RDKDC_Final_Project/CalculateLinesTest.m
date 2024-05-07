92c164ec1dd45c704e1811705b9e86bc04107ee0
andrewp0415 <81529989+andrewp0415@users.noreply.github.com>
2024-05-07 17:00:50 -0400

    updated calculate lines

--- RDKDC_Final_Project/CalculateLinesTest.m
+++ RDKDC_Final_Project/CalculateLinesTest.m
@@ -4,7 +4,7 @@
 
 %create two transformations
 g1 = [eye(3),[1;.5;.5];0,0,0,1];
-g4 = [eye(3),[1+.1*.707;.5-.1*.707;.75];0,0,0,1];
+g4 = [ROTX(pi/4),[1+.1*.707;.5-.1*.707;.75];0,0,0,1];
 
 frames = ur5_calculate_lines(g1,g4);
 
@@ -20,10 +20,24 @@ v2 = g2(1:3,4);
 v3 = g3(1:3,4);
 v4 = g4(1:3,4);
 
+%v1-v4 are the four points of the TOOL. let's find the points of the PEN.
+R67 = eye(3);
+p67 = [0; -49/1000; 115/1000];
+t67 = [R67, p67; 0, 0, 0, 1];
+
+% v1pen = t67*[v1;1];
+% v2pen = t67*[v2;1];
+% v3pen = t67*[v3;1];
+% v4pen = t67*[v4;1];
+
 % figure (1)
 % hold on
-% plotp3(v1)
-% plotp3(v2)
-% plotp3(v3)
-% plotp3(v4)
+% plotp3(v1,'b')
+% plotp3(v2,'b')
+% plotp3(v3,'b')
+% plotp3(v4,'b')
+% plotp3(v1pen(1:3),'r')
+% plotp3(v2pen(1:3),'r')
+% plotp3(v3pen(1:3),'r')
+% plotp3(v4pen(1:3),'r')
 % hold off
